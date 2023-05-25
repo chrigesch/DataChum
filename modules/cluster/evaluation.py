2056333af445157d5bebdf50a1376d2536246689
chrigesch <christianschetsche@psi.uba.ar>
2023-05-25 18:25:17 -0300

    feat: added box_plot to evaluation

--- modules/cluster/evaluation.py
+++ modules/cluster/evaluation.py
@@ -2,6 +2,7 @@
 from assets.colors import get_color
 
 # Import the required libraries
+import pandas as pd
 import plotly.express as px
 
 # Import libraries for debugging
@@ -9,6 +10,27 @@ from modules.cluster.main import clustering, clustering_cross_validation
 from modules.utils.load_and_save_data import read_csv
 
 
+def box_plot(
+    data: pd.Dataframe,
+    model_to_be_plotted: str,
+    score_to_be_plotted: str,
+):
+    list_colors = get_color(
+        "viridis",
+        len(data[data["model"] == model_to_be_plotted]["n_clusters"].dropna().unique()),
+    )
+
+    fig_variable = px.box(
+        data,
+        x="n_clusters",
+        y=score_to_be_plotted,
+        color_discrete_sequence=list_colors,
+        color="n_clusters",
+    )
+    fig_variable.update(layout_coloraxis_showscale=False)
+    fig_variable.update_layout(xaxis_type="category")
+    fig_variable.update_layout(width=600, height=400)
+    return fig_variable
 
 
 def main():
@@ -16,18 +38,25 @@ def main():
 
     results_clustering_cv = clustering_cross_validation(
         data=data,
-        imputation_numeric='most_frequent',
-        imputation_categorical='most_frequent',
-        scaler='zscore',
-        cluster_models=['AgglomerativeClustering_single',
-                        'SpectralClustering_nearest_neighbors'], # 'DBSCAN' | 'AgglomerativeClustering_single' | 'SpectralClustering_nearest_neighbors' | 'SpectralClustering_rbf' | AVAILABLE_MODELS_CLUSTER
+        imputation_numeric="most_frequent",
+        imputation_categorical="most_frequent",
+        scaler="zscore",
+        cluster_models=[
+            "AgglomerativeClustering_single",
+            "SpectralClustering_nearest_neighbors",
+        ],  # 'DBSCAN' | 'AgglomerativeClustering_single' | 'SpectralClustering_nearest_neighbors' | 'SpectralClustering_rbf' | AVAILABLE_MODELS_CLUSTER
         n_cluster_min=2,
         n_cluster_max=8,
-        classification_model=['GaussianNaiveBayes'], # 'GaussianNaiveBayes' | 'LightGBM'
+        classification_model=[
+            "GaussianNaiveBayes"
+        ],  # 'GaussianNaiveBayes' | 'LightGBM'
         inner_cv_folds=10,
-        inner_cv_rep=5)
+        inner_cv_rep=5,
+    )
 
-    results_clustering_cv.groupby(by=['model','n_clusters']).mean().sort_values(by='Silhouette', ascending=False).reset_index()
+    results_clustering_cv.results_cv.groupby(
+        by=["model", "n_clusters"]
+    ).mean().sort_values(by="Silhouette", ascending=False).reset_index()
 
     figure_to_plot = box_plot(results_clustering_cv)
 
