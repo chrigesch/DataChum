f71f848dafd6e441c727398edce660b4657da897
chrigesch <christianschetsche@psi.uba.ar>
2023-05-31 14:42:15 -0300

    docs: formatted index.py

--- index.py
+++ index.py
@@ -3,28 +3,36 @@ import streamlit as st
 from pages_ import Classification_and_Regression, Deployment, Exploratory_Data_Analysis
 
 # Page setup
-st.set_page_config(page_title='DataChum',
-                   page_icon='assets/logo_01.png',
-                   layout='wide')
+st.set_page_config(page_title="DataChum", page_icon="assets/logo_01.png", layout="wide")
 
 # Display the sidebar with a menu of apps
 with st.sidebar:
     # Add a title and intro text
-    st.title('Data Chum')
-    st.markdown('Your web app to analyze data')
+    st.title("Data Chum")
+    st.markdown("Your web app to analyze data")
 
     # Import image and add it to the sidebar
-    image = Image.open('assets/logo_01.png')
+    image = Image.open("assets/logo_01.png")
     st.image(image, use_column_width=True)
-    st.markdown('**Select an application from the list below**')
+    st.markdown("**Select an application from the list below**")
 
-    # Create a button for each application  
-    button_exploratory_data_analysis = st.button(label='**Exploratory Data Analysis**', type='secondary', use_container_width=True)
-    button_classification_and_regression = st.button(label='**Classification and Regression**', type='secondary', use_container_width=True)
-    button_deployment = st.button(label='**Deployment**', type='secondary', use_container_width=True)
+    # Create a button for each application
+    button_exploratory_data_analysis = st.button(
+        label="**Exploratory Data Analysis**",
+        type="secondary",
+        use_container_width=True,
+    )
+    button_classification_and_regression = st.button(
+        label="**Classification and Regression**",
+        type="secondary",
+        use_container_width=True,
+    )
+    button_deployment = st.button(
+        label="**Deployment**", type="secondary", use_container_width=True
+    )
 
 # Set Default page | Session state variables
-if 'page' not in st.session_state:
+if "page" not in st.session_state:
     st.session_state.page = Exploratory_Data_Analysis
 
 # Run the chosen app
