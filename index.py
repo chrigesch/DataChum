914a8b9b86a72cc201e55e0cc9734c909a52e5b2
chrigesch <christianschetsche@psi.uba.ar>
2023-06-04 13:37:59 -0300

    fix: added file uploader to index

--- index.py
+++ index.py
@@ -6,6 +6,10 @@ from pages_ import (
     Deployment,
     Exploratory_Data_Analysis,
 )
+from modules.utils.load_and_save_data import (
+    read_csv,
+    read_xlsx,
+)
 
 
 def main():
@@ -13,6 +17,22 @@ def main():
     st.set_page_config(
         page_title="DataChum", page_icon="assets/logo_01.png", layout="wide"
     )
+    # Create file uploader object
+    uploaded_file = st.file_uploader("Upload your database", type=["csv", "xlsx"])
+
+    # Set placeholder for data
+    if "data" not in st.session_state:
+        st.session_state.data = Exploratory_Data_Analysis
+    if uploaded_file is not None:
+        # Read the file to a dataframe using pandas
+        if uploaded_file.name[-3:] == "csv":
+            # Read in the csv file
+            st.session_state.data = read_csv(uploaded_file)
+        elif uploaded_file.name[-4:] == "xlsx":
+            # Read in the csv file
+            st.session_state.data = read_xlsx(uploaded_file)
+        else:
+            st.write("Type should be .CSV or .XLSX")
 
     # Display the sidebar with a menu of apps
     with st.sidebar:
