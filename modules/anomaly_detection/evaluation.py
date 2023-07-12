f4ad640df0c8c4fecd5493dde4e5835c5d16edc1
chrigesch <christianschetsche@psi.uba.ar>
2023-07-12 19:41:09 -0300

    feat: added method get_anomaly_scores

--- modules/anomaly_detection/evaluation.py
+++ modules/anomaly_detection/evaluation.py
@@ -1,7 +1,10 @@
 # Import moduls from local directories
 from assets.colors import get_color
+from modules.anomaly_detection.models import anomaly_detection_models_to_evaluate
+from modules.utils.preprocessing import data_preprocessing
 
 # Import the required Libraries
+import pandas as pd
 import plotly.express as px
 from plotly.subplots import make_subplots
 
@@ -51,3 +54,41 @@ def plot_anomalies_evaluation(
     )
     fig.update_layout(title_text=name_model, showlegend=False)
     return fig
+
+
+######################################
+# Private Methods / Helper functions #
+######################################
+
+
+def get_anomaly_scores(
+    data: pd.DataFrame,
+    imputation_numerical: str,
+    imputation_categorical: str,
+    scaler: str,
+    anomaly_detection_model: str,
+):
+    # Get categorical and numerical column names
+    cols_num = data.select_dtypes(include=["float", "int"]).columns.to_list()
+    cols_cat = data.select_dtypes(
+        include=["object", "category", "bool"]
+    ).columns.to_list()
+    pipeline = data_preprocessing(
+        cols_num=cols_num,
+        cols_cat=cols_cat,
+        imputation_numerical=imputation_numerical,
+        scaler=scaler,
+        imputation_categorical=imputation_categorical,
+        one_hot_encoding=True,
+    )
+    # Data preparation
+    data_prep = pipeline.fit_transform(data)
+
+    # Get list of model
+    anomaly_detection_model_list = anomaly_detection_models_to_evaluate(
+        models=[anomaly_detection_model]
+    )
+    anomaly_scores_min_max = (
+        anomaly_detection_model_list[0][1].fit(data_prep).predict_proba(data_prep)[:, 1]
+    )
+    return anomaly_scores_min_max
