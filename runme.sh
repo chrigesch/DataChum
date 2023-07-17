fc6eafa1520765250d5d5ea0a310909df6788011
chrigesch <christianschetsche@psi.uba.ar>
2023-07-17 17:10:25 -0300

    fix: removed runme.sh

--- runme.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-cd /home/ceci/Proyectos/DataChum
-source /home/ceci/miniconda3/bin/activate datachum_39
-nohup streamlit run index.py >/dev/null 2>&1 &
-xdg-open http://192.168.1.4:8501
